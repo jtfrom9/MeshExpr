58bc8b073c4805a7b81a12b915d2f24c224101f9
jtfrom9 <jun.tachikawa@gmail.com>
2020-07-29 17:04:27 +0900

    init

---
+++ Assets/MeshGen.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MeshGen : MonoBehaviour
+{
+    void Awake()
+    {
+        var mesh = new Mesh();
+
+        var vertices = new List<Vector3> {
+            //　反時計回り (裏側)
+            // new Vector3 (1, 0, 0),
+            // new Vector3 (0, 1, 0),
+            // new Vector3 (-1, 0, 0),
+
+            // 時計回り (表)
+            new Vector3 (-1, 0, 0), // 0
+            new Vector3 (0, 1, 0),  // 1
+            new Vector3 (1, 0, 0),  // 2
+        
+            // new Vector3 (-1, 0, 0), // 0
+            // new Vector3 (1, 0, 0), // 2
+
+            new Vector3 (0, -1, 0.5f) // 3
+        };
+        mesh.SetVertices(vertices);
+
+        var triangles = new List<int> { 0, 1, 2, 0, 2, 3 };
+        // var triangles = new List<int> { 0, 1, 2, 3, 4, 5 };
+        mesh.SetTriangles(triangles, 0);
+
+        // 法線計算。これでライトが考慮される（色がつく. material次第)
+        mesh.RecalculateNormals();
+
+        foreach(var (s,index) in mesh.EachVerticiesInfo().Select((s,i)=>(s,i))) {
+            Debug.Log($"[{index}] {s}");
+        }
+
+        // Debug.Log($"verticies={string.Join(",", mesh.vertices)}");
+        // Debug.Log($"normals={string.Join(",", mesh.normals)}");
+        // Debug.Log($"triangles={string.Join(",",mesh.triangles)}");
+
+        var meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh = mesh;
+    }
+}
---
+++ Assets/MeshLog.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MeshDebugExtension
+{
+    public static IEnumerable<string> EachVerticiesInfo(this Mesh mesh)
+    {
+        var triindecies = mesh.GetTriangles(0);
+        int numTriangle = triindecies.Length / 3;
+
+        var vertices = new List<Vector3>();
+        mesh.GetVertices(vertices);
+        var normals = new List<Vector3>();
+        mesh.GetNormals(normals);
+        var uvs = new List<Vector2>();
+        mesh.GetUVs(0, uvs);
+
+        for (int tri = 0; tri < numTriangle; tri++) {
+            var sb = new StringBuilder();
+            sb.AppendFormat("Tri[{0}]\n", tri);
+            for (int i = 0; i < 3; i++) {
+                var vi = triindecies[tri * 3 + i];
+                sb.AppendFormat("  V={0} ({1}), N={2}, Uv={3}\n",
+                    vi,
+                    vertices[vi].ToString(),
+                    normals[vi].ToString(),
+                    (uvs.Count > vi) ? uvs[vi].ToString() : "");
+            }
+            yield return sb.ToString();
+        }
+        // foreach (var vi in triangles)
+        // {
+        //     yield return string.Format("index={0} ({1}), normal={2}",
+        //         vi,
+        //         mesh.vertices[vi].ToString(),
+        //         mesh.normals[vi].ToString());
+        // }
+    }
+}
+
+public class MeshLog : MonoBehaviour
+{
+    void PrintInfo(Mesh mesh)
+    {
+        var sb = new StringBuilder();
+        foreach (var (s, index) in mesh.EachVerticiesInfo().Select((s, i) => (s, i)))
+        {
+            sb.AppendFormat("[{0}] {1}\n", index, s);
+        }
+        Debug.Log($"<{gameObject.name}> Vertecies={mesh.vertexCount} \n{sb.ToString()}");
+    }
+
+    void Start()
+    {
+        var mf = GetComponent<MeshFilter>();
+        PrintInfo(mf.mesh);
+    }
+}
